93a7ef5de7e5c3db2872f0149e438c9717cc25d3
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-06-15 17:18:10 -0300

    create archives to simulate database

---
+++ src/Database/Categorias.js
@@ -0,0 +1,10 @@
+export default class Categorias {
+
+    constructor() {
+        this.categorias = ["Musica", "Games", "Esportes", "Internet", "Trabalho"];
+    }
+
+    adicionarCategoria(novaCategoria) {
+        this.categoria.push(novaCategoria);
+    }
+}
